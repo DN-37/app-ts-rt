2ecf666a55ef3243072392bb25af2122a8870a0a
Danila Nefedov <dn375@mail.ru>
2023-05-07 12:37:17 +0400

    created component Background and added styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,17 @@
 import React from 'react';
+import Navbar from './sections/Navbar';
+import Footer from './sections/Footer';
+import Background from './components/Background';
 
 function App() {
   return (
-    <div className='main-container'>App</div>
+    <div className='main-container'>
+      <Background />
+      <div className="app">
+        <Navbar />
+        <Footer />
+      </div>
+    </div>
   )
 }
 
