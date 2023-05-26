5a89fdd8a2cf1375924288529adfc9b61174a9e4
Danila Nefedov <dn375@mail.ru>
2023-05-26 20:23:02 +0400

    added useeffect for get pokemons and created util pokemon image

--- src/pages/Search.tsx
+++ src/pages/Search.tsx
@@ -1,13 +1,25 @@
 import React, { useEffect } from 'react';
 import Wrapper from "../sections/Wrapper";
-import { useAppDispatch } from "../app/hooks";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
 import { getInitialPokemonData } from "../app/reducers/getInitialPokemonData";
+import { getPokemonsData } from '../app/reducers/getPokemonsData';
 
 function Search() {
     const dispatch = useAppDispatch();
+    const { allPokemon } = useAppSelector(({ pokemon }) => pokemon);
     useEffect(() => {
         dispatch(getInitialPokemonData());
     }, [dispatch]);
+
+    useEffect(() => {
+        if (allPokemon) {
+            const clonedPokemon = [...allPokemon];
+            const randomPokemonsId = clonedPokemon
+                .sort(() => Math.random() - Math.random())
+                .slice(0, 20)
+            dispatch(getPokemonsData(randomPokemonsId))
+        }
+    }, [allPokemon, dispatch])
 
     return (
         <div>Search</div>
